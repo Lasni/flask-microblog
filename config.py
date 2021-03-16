af4ffdc485c0506b0913b9c533465e2190e768fc
lasni <grega.lasnibat@gmail.com>
2021-03-16 12:21:16 +0100

    pagination

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config(object):
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 5
     MAIL_SERVER = None  # os.environ.get("MAIL_SERVER")
     # MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     # MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
