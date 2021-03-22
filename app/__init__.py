1dcfe49d102cc928dc9ab86da86f7622d3facb57
lasni <grega.lasnibat@gmail.com>
2021-03-22 11:58:04 +0100

    refactoring with flask Blueprint pt.2 (error when resetting password)

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
-    login.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
-from app import app, db, login_manager
-from time import time
-import jwt
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
+from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login_manager
 
 # followers association table for a many-to-many User relationship
 followers = db.Table(
@@ -39,9 +40,9 @@ class User(UserMixin, db.Model):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
-                "reset_password"
-            ]
+            id = jwt.decode(
+                token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
+            )["reset_password"]
         except:
             return
         return User.query.get(id)
@@ -59,7 +60,7 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {"reset_password": self.id, "exp": time() + expires_in},
-            app.config["SECRET_KEY"],
+            current_app.config["SECRET_KEY"],
             algorithm="HS256",
         )
 
