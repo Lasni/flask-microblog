8a5c1809e8b78e44f7fe5721972b0e833395edf8
lasni <grega.lasnibat@gmail.com>
2021-04-08 15:46:06 +0200

    background jobs (pt.5)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
-from flask_babel import lazy_gettext as _l
+from flask_babel import _, lazy_gettext as _l
 
 
 class LoginForm(FlaskForm):
